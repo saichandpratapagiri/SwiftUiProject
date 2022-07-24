50dc803b0661fcb29df6206b546c17c4b18f4136
Saichand pratapagiri <saichand.pratapagiri@gmail.com>
2022-07-24 20:11:15 +0530

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Saichand Pratapagiri on 24/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
